919da8995523de5b5a1c96d26e05b40782a0d75c
itgl <itgel0518@icloud.com>
2022-10-03 11:07:15 +0800

    improvement

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
+const axios = require('axios');
 
 const {google} = require('googleapis');
 const GOOGLE_CLIENT_ID = '378382094535-nkiagmh7esjcuidclb7625lur4d2b2uc.apps.googleusercontent.com'
@@ -233,7 +234,62 @@ app.post('/getGoogleTime', async (req,res,next) => {
     next(error)
   }
 })
+const stsBase = 'https://test.hipay.mn';
+// const client_secret = '5hPR4fs9g2Wq5ZAXWI0L2L';
+// const client_id = 'amitawlc';
+const client_secret = 'Trk4UNHt58LqDwRL4adsXV'
+const client_id = 'amita001';
+const bearer = 'Bearer ' + client_secret;
 
+
+app.post('/getToken', async (req,res,next) => {
+  const code = req.body.code;
+  console.log(code)
+  data = {
+    client_id: client_id,
+    client_secret: client_secret,
+    redirect_uri: 'https://amita-backend.herokuapp.com/webhook',
+    code: code,
+    grant_type: 'authorization_code'
+  }
+  const response = await axios.post(stsBase+'/v2/auth/token', data)
+  const result = response.data;
+  console.log(result);
+  res.send(result);
+})
+
+app.post('/getUserInfo', async (req,res,next) => {
+  const token = req.body.token;
+  //const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBOUJENzBCMDc5OEE3NUY3RTA1MzJBNjRBOEMwOUIzQiIsImlzcyI6ImFtaXRhMDAxIiwicmZ0IjoiRUExOEZDOTMxMzgzMjEyQUUwNTMyQTY1QThDMEJDMDkiLCJleHAiOjE2NjQ3Njc5NTl9._iW8eXb9VDXzxARQaY6ple8chHOgrg1agBIScyj9t08';
+  const bearer1 = 'Bearer ' + token;
+  const response = await axios.get(stsBase+'/v2/user/info',
+    {
+      headers: {
+        Authorization: bearer1
+      }
+    })
+  const result = response.data;
+  console.log(result);
+  res.send(result);
+})
+
+app.get('/checkout', async (req,res,next) => {
+  //const code = req.body.code;
+  const response = await axios.post(stsBase+'/checkout',{
+    entityId: client_id,
+    amount: '5000',
+    currency: 'MNT',
+    redirect_uri: 'https://amita-front.herokuapp.com/access'
+  },
+  {
+    headers: {
+      Authorization: bearer
+    }
+  })
+  const result = response.data;
+  console.log(result);
+  res.send(result);
+})
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
