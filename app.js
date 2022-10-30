f0e6b552faa46425b488e51c656a47d6858fe6a3
itgl <itgel0518@icloud.com>
2022-10-30 23:48:10 +0800

    test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const {google} = require('googleapis');
 const GOOGLE_CLIENT_ID = '378382094535-nkiagmh7esjcuidclb7625lur4d2b2uc.apps.googleusercontent.com'
 const GOOGLE_CLIENT_SECRED = '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'
-const REFRESH_TOKEN = '1//04ObcA2Et7yq_CgYIARAAGAQSNwF-L9IrzpEOnJOgNKx1xKUejZTuEMLzL_rXCi5MnSWwk2oibiAMwjsHgu-X0qhDmHjoBjs6e5Q'
+const REFRESH_TOKEN = '1//04lCL4uKtjM5oCgYIARAAGAQSNwF-L9IrX3bswl7z3ySCNtJNK9ZVU3uH1NTuDYgJbO5yoFD0I-WLbm2oAFxUILJ_F8VO7o3BWhc'
 const calendarID = 'amurang123@gmail.com'
 
 //378382094535-nkiagmh7esjcuidclb7625lur4d2b2uc.apps.googleusercontent.com  -> client id
